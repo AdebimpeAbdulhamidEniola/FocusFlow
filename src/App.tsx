dfb3e71bff985cc228df32efc6a21312a99eb6dd
AdebimpeAbdulhamidEniola <adebimpeabdulhamid5@gmail.com>
2025-11-06 07:00:17 +0100

    start working on FocusFlow

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import MainTimerPage from "./components/MainTimerPage"
+// import Settings from "./components/Settings"
+// import TaskManagement from "./components/TaskManagement"
+
+const App = () => {
+  return (
+    <div>
+      <MainTimerPage />
+      {/* <Settings />
+      <TaskManagement /> */}
+    </div>
+  )
+}
+
+export default App
