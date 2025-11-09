5a01eff071a97c899bfb6987daa2f9be4c7c35da
AdebimpeAbdulhamidEniola <adebimpeabdulhamid5@gmail.com>
2025-11-09 15:35:56 +0100

    added new states and props

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,66 @@
-import MainTimerPage from "./components/MainTimerPage"
+import MainTimerPage from "./components/MainTimerPage";
 // import Settings from "./components/Settings"
 // import TaskManagement from "./components/TaskManagement"
+import { useState, useEffect } from "react";
+import type { SessionType } from "./Type";
 
 const App = () => {
+  const [isRunning, setIsRunning] = useState(false);
+  const [timeRemaining, setTimeRemaining] = useState(30);
+  const [sessionType, setSessionType] = useState<SessionType | null>(null);
+  const [pomodoroCount, setPomodoroCount] = useState(0);
+  const [hasStarted, setHasStarted] = useState(false);
+
+
+  useEffect(() => {
+    if (!isRunning) return;
+
+    const interval = setInterval(() => {
+      setTimeRemaining((prev) => {
+        const newTime = prev - 1;
+        console.log(`Time remaining is ${newTime}`);
+
+        if (newTime === 0) {
+          if (sessionType === "focus") {
+            setPomodoroCount((prevCount) => prevCount + 1);
+            setSessionType("shortBreak");
+          } else {
+            setSessionType("focus");
+          }
+        }
+
+        if (pomodoroCount === 4) {
+          setSessionType("longBreak");
+        }
+
+        return newTime;
+      });
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, [isRunning, sessionType, pomodoroCount]);
+
+  const toggleStart = () => {
+  if (!hasStarted) {
+    setHasStarted(true);
+    setIsRunning(true);  
+  } else {
+    setIsRunning(prev => !prev); 
+  }
+};
+
   return (
     <div>
-      <MainTimerPage />
+      <MainTimerPage
+        isRunning={isRunning}
+        timeRemaining={timeRemaining}
+        toggleStart={toggleStart}
+        hasStarted = {hasStarted}
+      />
       {/* <Settings />
       <TaskManagement /> */}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/components/MainTimerPage.tsx
+++ src/components/MainTimerPage.tsx
@@ -1,35 +1,44 @@
 import NavigationComponent from "./NavigationComponent";
 import { RotateCcw, SkipForward } from "lucide-react";
 
-const MainTimerPage = () => {
+const MainTimerPage = ({
+  isRunning,
+  timeRemaining,
+  toggleStart,
+  hasStarted,
+}: {
+  isRunning: boolean;
+  timeRemaining: number;
+  toggleStart: () => void;
+  hasStarted: boolean;
+}) => {
+  const minutes = Math.floor(timeRemaining / 60);
+  const seconds = timeRemaining % 60;
+  const formattedTime = `${minutes.toString().padStart(2, "0")}:${seconds
+    .toString()
+    .padStart(2, "0")}`;
+
   return (
     <div className="bg-[#121c21] max-w-screen min-h-screen py-5 font-[Inter,sans-serif] text-white">
       <NavigationComponent />
 
       <div className="bg-[#1A2B33] flex justify-between lg:w-[50%] w-[80%] mx-auto rounded-lg mt-10 px-4 py-1">
-        <button
-          className={`p-4 basis-[33%]`}
-          id="focus"
-        >
+        <button className={`p-4 basis-[33%]`} id="focus">
           Focus
         </button>
 
-        <button
-          className={`p-4  basis-[33%]`}
-          id="short_break"
-        >
+        <button className={`p-4  basis-[33%]`} id="short_break">
           Short Break
         </button>
 
-        <button
-          className={`p-4  basis-[33%]`}
-          id="long_break"
-        >
+        <button className={`p-4  basis-[33%]`} id="long_break">
           Long Break
         </button>
       </div>
 
-      <div className="w-40 h-40 border-12 border-[#12a3ed] mx-auto my-24 rounded-full"></div>
+      <div className="w-50 h-50  border-12 border-[#12a3ed] mx-auto my-10 rounded-full text-5xl text-white font-bold flex place-content-center">
+        <p className="m-auto text-center p-4 ">{formattedTime}</p>
+      </div>
 
       <div className="control-buttons flex gap-5 justify-center">
         <div className="reset">
@@ -37,9 +46,12 @@ const MainTimerPage = () => {
             <RotateCcw size={22} />
           </button>
         </div>
-
-        <button className="bg-[#12a3ed] px-9 py-2 rounded-md hover:bg-[#1bb7ff] focus:bg-[#1bb7ff] transition-all cursor-pointer">
-          Start
+       
+        <button
+          className="bg-[#12a3ed] px-9 py-2 rounded-md hover:bg-[#1bb7ff] focus:bg-[#1bb7ff] transition-all cursor-pointer"
+          onClick={toggleStart}
+        >
+           {!hasStarted ? "Start" : isRunning ? "Pause" : "Resume"}
         </button>
 
         <div className="skip">
